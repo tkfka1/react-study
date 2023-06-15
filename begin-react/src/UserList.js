98037de2dd53ae5ba5b831b3c5c9c392a8645fb6
tkfka1 <36651040+tkfka1@users.noreply.github.com>
2023-06-15 12:43:53 +0900

    배열에 항목 수정하기

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,21 +1,34 @@
 import React from 'react';
 
-function User({ user, onRemove }) {
+function User({ user, onRemove, onToggle }) {
   return (
     <div>
-      <b>{user.username}</b> <span>({user.email})</span>
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black'
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
 }
 
-//jsx 외부에서 정의되어진 event hander나 callback로 parameter를 pass할 때 arrow function을 씁니다.
-
-function UserList({ users, onRemove }) {
+function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
       {users.map(user => (
-        <User user={user} key={user.id} onRemove={onRemove} />
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
