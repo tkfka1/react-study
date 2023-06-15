5dda525e0be68bbf2834d879a5c257a8901b29a6
tkfka1 <36651040+tkfka1@users.noreply.github.com>
2023-06-15 11:06:37 +0900

    props 를 통해 컴포넌트에게 값 전달, 조건부랜더링

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import Hello from './Hello';
+import Wrapper from './Wrapper';
+
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24, // 기본 단위 px
-    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
-  }
-
   return (
-    <>
-      <Hello />
-      <div style={style}>{name}</div>
-    </>
-  );
+    <Wrapper>
+      <Hello name="react" color="red" isSpecial={true}/>
+      <Hello color="pink" />
+    </Wrapper>
+  )
 }
 
 export default App;
--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-function Hello() {
-  return <div>안녕하세요</div>
+function Hello({ color, name, isSpecial }) {
+  return (
+    <div style={{ color }}>
+      { isSpecial ? <b>*</b> : null }
+      안녕하세요 {name}
+    </div>
+  );
+}
+
+// isSpecial 값이 true 라면 <b>*</b> 를, 그렇지 않다면 null 을 렌더링합니다.
+
+Hello.defaultProps = {
+  name: '이름없음'
 }
 
 export default Hello;
